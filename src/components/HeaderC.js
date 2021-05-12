a88e1d4b0a7a14e9cef41423a758771c5ffcdc2d
Ahmed BARGADY <bargadyahmed2000@gmail.com.com>
2021-05-12 18:19:52 +0000

    adding data.

--- src/components/HeaderC.js
+++ src/components/HeaderC.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function HeaderC() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return <div></div>;
 }
 
-export default HeaderC
+export default HeaderC;
